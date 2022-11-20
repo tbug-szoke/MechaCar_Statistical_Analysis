0787f41c324dfe20483ae13ee78e17e8f6fb9598
tbug-szoke <106888285+tbug-szoke@users.noreply.github.com>
2022-11-20 12:03:10 -0500

    Added summary stats on suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,9 @@ mpg <- read.csv(file='MechaCar_mpg.csv')
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg)) #generate summary statistics
+
+library(tidyverse)
+lots <- read.csv(file='Suspension_Coil.csv')
+total_summary <- lots %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #create dataframe with summary statistics on PSI
+lot_summary <- lots %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #create dataframe with summary statistics on PSI by lot
+
